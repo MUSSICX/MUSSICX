f6846c53d9ead73558dcc35561de832b2adf9ae3
MUSSICX <152394331+MUSSICX@users.noreply.github.com>
2024-07-01 20:55:41 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "LEMONADE",
+    artist: "DILJEET DOSAN",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "8 ASLE",
+    artist: "SUKHA",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "WE-ROLLIN",
+    artist: "SHUBH",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "TERE HAWALE",
+    artist: "ARIJIT SINGH",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "SPAIN",
+    artist: "JASSA DHILLON",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "WINNING SPEECH",
+    artist: "KARAN AUJLA",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "CHALEYA",
+    artist: "ARIJIT SINGH",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "NAINA(CREW)",
+    artist: "DILJIT&BADSHAH",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "NAAH",
+    artist: "JASS MANAK",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "KING SHIT",
+    artist: "SHUBH",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
